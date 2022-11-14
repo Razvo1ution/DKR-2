78c5a3f8dab42913f10eb81b780a7e0b8637061e
Razvo1ution <117993907+Razvo1ution@users.noreply.github.com>
2022-11-14 22:14:22 +0300

    Add files via upload

---
+++ DKR 2/DKR(1).pas
@@ -0,0 +1,33 @@
+﻿type mas=array[1..100] of integer;
+begin
+var 
+  x,a:mas;
+  var
+f:boolean;
+
+ var n:= readbyte('Введите количество элементов');
+ 
+ print('А теперь введите ',n,' элементов массива');
+  for var i:=1 to n do
+  read(a[i]);
+  readln;
+ 
+  while (i mod 2=0) do begin
+  f:=true;
+   if i mod 2=0 then
+    if a[i]=a[i+1] then f:=false
+    else
+    if a[i]>a[i+2] then f:=true
+    else
+     f:=false
+ 
+    else if a[i]<a[i+2] then f:=true
+     else f:=false;
+     i:=i+1;
+     end;
+ 
+  if f=true then print('Подпоследовательность является монотонной')
+    else
+     print('Подпоследовательность не является монотонной');
+     readln;
+     end.
---
+++ DKR 2/dkr 2.pas
@@ -0,0 +1,24 @@
+﻿begin
+var a,b:string; 
+ a:= readstring('введите исходную строку');
+ b:= readstring('введите вторую строку');
+ var i:=1;
+  var c:=length(a);
+  var d:=length(b);
+  var g:=false;
+  while i<=c-d+1 do
+  begin
+    g:=true;
+    for var f:=1 to d -1 do
+      if a[i+f]<>b[f+1]then
+      begin
+        i:=i+f;
+        g:=false;
+      end;
+      if g then
+       print('является')
+      else
+        println('не является');
+      i:=i+1;
+  end;
+end.
